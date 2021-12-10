d4c3fb1008100a371f8755f4c4401f89f39b62c0
Julia <tsapikova@mail.ru>
2021-12-10 13:36:52 +0400

    first commit

---
+++ app/src/main/java/com/julia/finalproject/MainActivity.kt
@@ -0,0 +1,120 @@
+package com.julia.finalproject
+
+
+import net.objecthunter.exp4j.ExpressionBuilder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.Expression
+
+
+
+class MainActivity : AppCompatActivity()
+{
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        tvOne.setOnClickListener {
+            evaluateExpression("1", clear = true)
+        }
+
+        tvTwo.setOnClickListener {
+            evaluateExpression("2", clear = true)
+        }
+
+        tvThree.setOnClickListener {
+            evaluateExpression("3", clear = true)
+        }
+        tvFour.setOnClickListener {
+            evaluateExpression("4", clear = true)
+        }
+
+        tvFive.setOnClickListener {
+            evaluateExpression("5", clear = true)
+        }
+
+        tvSix.setOnClickListener {
+            evaluateExpression("6", clear = true)
+        }
+
+        tvSeven.setOnClickListener {
+            evaluateExpression("7", clear = true)
+        }
+
+        tvEight.setOnClickListener {
+            evaluateExpression("8", clear = true)
+        }
+
+        tvNine.setOnClickListener {
+            evaluateExpression("9", clear = true)
+        }
+
+        tvZero.setOnClickListener {
+            evaluateExpression("0", clear = true)
+        }
+
+
+        tvPlus.setOnClickListener {
+            evaluateExpression("+", clear = true)
+        }
+
+        tvMinus.setOnClickListener {
+            evaluateExpression("-", clear = true)
+        }
+
+        tvMul.setOnClickListener {
+            evaluateExpression("*", clear = true)
+        }
+
+        tvDivide.setOnClickListener {
+            evaluateExpression("/", clear = true)
+        }
+
+        tvDot.setOnClickListener {
+            evaluateExpression(".", clear = true)
+        }
+
+        tvClear.setOnClickListener {
+            tvExpression.text = ""
+            tvResult.text = ""
+        }
+
+        tvEquals.setOnClickListener {
+            val text = tvExpression.text.toString()
+            val expression = ExpressionBuilder(text).build()
+
+            val result = expression.evaluate()
+            val longResult = result.toLong()
+            if (result == longResult.toDouble()) {
+                tvResult.text = longResult.toString()
+            } else {
+                tvResult.text = result.toString()
+            }
+        }
+
+        tvBack.setOnClickListener {
+            val text = tvExpression.text.toString()
+            if(text.isNotEmpty()) {
+                tvExpression.text = text.dropLast(1)
+            }
+
+            tvResult.text = ""
+        }
+    }
+
+    private fun evaluateExpression(string: String, clear: Boolean) = if(clear) {
+        tvResult.text= ""
+        tvExpression.append(string)
+    } else {
+        tvExpression.append(tvResult.text)
+        tvExpression.append(string)
+        tvResult.text = ""
+    }
+}
+
+
+
